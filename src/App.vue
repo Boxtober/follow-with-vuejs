a3ce5a4f5d973e914a9ff0b1d2cc0aa0f6d90e23
Sabrina Affane <contact@boxtober.fr>
2023-01-03 09:28:22 +0100

    projet vuejs V1 03/01/23

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <img id="logo" alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to my site !"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+body {
+  background: linear-gradient(0deg, rgb(182, 181, 214) 0%, rgba(255,255,255,1) 100%);
+}
+
+
+button {
+  background-color: #EA4C89;
+  border-radius: 8px;
+  border-style: none;
+  box-sizing: border-box;
+  color: #FFFFFF;
+  cursor: pointer;
+  display: inline-block;
+  font-family: "Haas Grot Text R Web", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  height: 40px;
+  line-height: 20px;
+  list-style: none;
+  margin: 0;
+  outline: none;
+  padding: 10px 16px;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  transition: color 100ms;
+  vertical-align: baseline;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+}
+
+button:hover,
+button:focus {
+  background-color: #F082AC;
+}
+
+#app {
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;  */
+  font-family: 'Raleway', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#logo {
+
+  width: 200px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p v-if="abonnes == 0">Aucun abonnés :'(</p>
+    <p v-else-if="abonnes == 1">Une personne s'est abonnée !</p>
+    <p v-else>{{abonnes}} personnes sont abonnées 🎉</p>
+    <button v-on:click="subscribe()">S'abonner !</button>
+    <div class="comment" v-for="comment, idx in comments" v-bind:key="idx">
+      <h3>{{comment.nom}}</h3>
+      <p>{{comment.commentaire}}</p>
+
+      <div v-if="comment.note > 1">
+        <span v-for="i in comment.note" v-bind:key="i">⭐</span>
+      </div>
+      <span v-else>🌟</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data: function () {
+    return {
+      abonnes: 0,
+      comments: [{
+        nom : 'Toto',
+        commentaire: "blablabla",
+        note : 3,
+      },
+      {
+        nom : 'Titi',
+        commentaire: "le ciel est bleu",
+        note : 5,
+      },
+      {
+        nom : 'Grominet',
+        commentaire: "Sabristi-saucisse !",
+        note : 1,
+      }
+    
+    ],
+    }
+  },
+  methods: {
+    subscribe: function () {
+      this.abonnes++;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+p {
+  font-size: 22px;
+}
+
+
+</style>
